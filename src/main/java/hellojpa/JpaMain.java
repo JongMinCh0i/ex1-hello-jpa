b193d0f7b3f899190bc5e78600b2f616d9325994
JongMinCh0i <wonderland314592@gmail.com>
2023-10-02 14:44:04 +0900

    2.1 영속성 컨텍스트1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,8 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.xml.transform.Result;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,21 +15,18 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 2L);
-//            findMember.setName("HelloJPA");
 
-            // 대상이 테이블이 아닌 멤버 객체
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(4)
-                    .getResultList();
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
 
             tx.commit();
-
         } catch (Exception e) {
             tx.rollback();
         } finally {
