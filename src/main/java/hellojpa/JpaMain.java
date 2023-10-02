248510c2ed28fd6b7b52c0b968af79527d8b3a1f
JongMinCh0i <wonderland314592@gmail.com>
2023-10-02 16:19:53 +0900

    2.4 준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,13 +17,14 @@ public class JpaMain {
         try {
 
             //영속
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+
+            // 준 영속성 상태로 전환
+            em.detach(member);
 
             System.out.println("==================");
-            em.flush();
-
-
+            // 데이터를 변경했는데도 불구 하고 Update 쿼리가 실행되지 않음
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
